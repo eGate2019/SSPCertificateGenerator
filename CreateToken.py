5d2892d40afc0bc20282637282ba26dcd570cb06
eGate2019 <alain.rhelimi@free.fr>
2021-03-20 09:42:12 +0100

    token verif

--- CreateToken.py
+++ CreateToken.py
@@ -1,17 +1,28 @@
-import datetime
+
 import uuid
+
 
 import asn1tools
 import yaml
 from cryptography import x509
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import asymmetric, hashes, serialization
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import ec
-from cryptography.x509.oid import NameOID
+
+from pyasn1.codec.der import decoder, encoder
+from pyasn1.type import univ, namedtype
 
 import constante as cts
 from CreateCertificate import PrivateKey, PublicKey
 from ui import UI
+
+
+class AuthenticationToken(univ.Sequence):
+    componentType = namedtype.NamedTypes(
+        namedtype.NamedType('tbsToken', univ.Sequence()),
+        namedtype.NamedType('signatureAlgorithm', univ.Sequence()),
+        namedtype.NamedType('signature', univ.Sequence())
+    )
 
 
 class RawKey:
@@ -38,8 +49,8 @@ class SSPtoken:
     def setModel(self, modeles):
         """Set the ASN.1 model."""
         self.model = asn1tools.compile_files(modeles, 'der')
-        for typ in self.model.types:
-            print(typ)
+        # for typ in self.model.types:
+        #     print(typ)
 
     def generate(self, token_parameter):
         """ Generate a token according to a set of parameters."""
@@ -49,35 +60,55 @@ class SSPtoken:
             self.setModel(token_parameter['modeles'])
             self.token_name = token_parameter['Name']
             # Getting of the token public key.
-            private_key = ec.generate_private_key(ec.BrainpoolP256R1)
-            public_key = private_key.public_key()
-            public_key_der = public_key.public_bytes(
+            private_ekey = ec.generate_private_key(ec.BrainpoolP256R1)
+            private_ekey_der = private_ekey.private_bytes(
+                encoding=serialization.Encoding.DER,
+                format=serialization.PrivateFormat.TraditionalOpenSSL,
+                encryption_algorithm=serialization.NoEncryption()
+                )
+            with open("private_keys/" + self.token_name+"-private-key.der", "wb") as f:
+                f.write(private_ekey_der)
+
+            public_ekey = private_ekey.public_key()
+            public_key_der = public_ekey.public_bytes(
                 encoding=serialization.Encoding.DER,
                 format=serialization.PublicFormat.SubjectPublicKeyInfo
             )
-            public_key_data= self.model.decode('SubjectPublicKeyInfo', public_key_der)
+            public_key_data = self.model.decode(
+                'SubjectPublicKeyInfo', public_key_der)
             cert = x509.CertificateBuilder()
-            cert = cert.public_key(public_key)
+            cert = cert.public_key(public_ekey)
             # Collection of the subjet attributes
             for k, m_field in token_parameter.items():
 
                 if k == "issuer":
                     # Get the issuer private key.
-                    self.issuer_private_key = PrivateKey(m_field),
-                    self.issuer_public_key = PublicKey(m_field)
+                    self.issuer_private_key = PrivateKey(m_field).get()
+                    self.issuer_public_key = PublicKey(m_field).get()
 
             atbsToken = {'version': 0}
+            atbsToken['signature'] = {}
+            atbsToken['signature']['algorithm'] = cts.OID_ECDSASHA256
             atbsToken['subjectPublicKeyInfo'] = public_key_data
             aRand = uuid.uuid4()
             atbsToken['aATK-Content'] = {
                 'aChallenge': aRand.bytes}
             atbsToken['aATK-Content']['aKey-Size'] = cts.KEY_SIZE_E128  # 'Key-Size e128'
             atbsToken['aATK-Content']['aStreamCipherIdentifier'] = cts.AES_CGM  # 'aAES-CGM-StreamCipherIdentifier'
+
+            m_AKI = x509.AuthorityKeyIdentifier.from_issuer_public_key(
+                self.issuer_public_key)
+            atbsToken['extensions'] = [{}]
+            atbsToken['extensions'][0]['extnID'] = cts.OID_AKI
+            atbsToken['extensions'][0]['critical'] = True
+            atbsToken['extensions'][0]['extnValue'] = m_AKI.key_identifier
+
             tbsToken = self.model.encode('TBSToken', atbsToken)
-            signature_der = private_key.sign(
+            signature_der = self.issuer_private_key.sign(
                 tbsToken, ec.ECDSA(hashes.SHA256()))
             signature_data = self.model.decode(
                 'ECDSA-Sig-Value', signature_der)
+
             auth_token = {}
             auth_token['tbsToken'] = atbsToken
             auth_token['signature'] = signature_data
@@ -89,7 +120,57 @@ class SSPtoken:
             with open("./tokens/"+self.path+"_" +
                       self.token_name+".der", "wb") as f:
                 f.write(auth_token_der)
+            self.verify(token_parameter)  
+        except ValueError as e:
+            print("Oops!..", e)
 
+    def verify(self, token_parameter):
+        """ Generate a token according to a set of parameters."""
+        try:
+            # Creation of the token builder
+            self.setModel(token_parameter['modeles'])
+            self.token_name = token_parameter['Name']
+            for k, m_field in token_parameter.items():
+
+                if k == "issuer":
+                    # Get the issuer private key.
+                    self.issuer_public_key = PublicKey(m_field).get()         
+            subjectKeyIdentifier = x509.SubjectKeyIdentifier.from_public_key(self.issuer_public_key)
+            auth_token_der = 0
+            # Write our token out to disk.
+            with open("./tokens/"+self.path+"_" +
+                      self.token_name+".der", "rb") as f:
+                auth_token_der = f.read()
+            token_verif = self.model.decode('AuthenticationToken',
+                                            auth_token_der
+                                            )
+            if token_verif['tbsToken']['version'] != cts.V1:
+                raise Exception("wrong Version")
+
+            if token_verif['tbsToken']['signature']['algorithm'] != cts.OID_ECDSASHA256:
+                raise Exception("wrong Signature algorithm")         
+
+            if token_verif['tbsToken']['aATK-Content']['aStreamCipherIdentifier'] not in [cts.AES_CGM]:
+                raise Exception("wrong stream cipher identifier")
+
+            if token_verif['tbsToken']['aATK-Content']['aKey-Size'] not in [cts.KEY_SIZE_E128, cts.KEY_SIZE_E256]:
+                raise Exception("wrong Key size")
+
+            m_AKI = b'x00'
+            for extension in token_verif['tbsToken']['extensions']:
+                if extension['extnID'] == cts.OID_AKI:
+                    m_AKI = extension['extnValue']
+
+            if subjectKeyIdentifier.digest != m_AKI:
+                raise Exception("wrong AKI")
+
+            value = decoder.decode(
+                auth_token_der, asn1Spec=AuthenticationToken()
+            )
+            self.issuer_public_key.verify(
+                encoder.encode(value[0].getComponentByPosition(2)),
+                encoder.encode(value[0].getComponentByPosition(0)),
+                ec.ECDSA(hashes.SHA256()))
         except ValueError as e:
             print("Oops!..", e)
 
--- constante.py
+++ constante.py
@@ -4,3 +4,5 @@ KEY_SIZE_E128 = 0   # Key_Size e128
 KEY_SIZE_E256 = 1  # Key_Size e256
 AES_CGM = 0  # aAES-CGM-StreamCipherIdentifier
 OID_ECDSASHA256 = '1.2.840.10045.4.3.2'  # ecdsa-with-SHA256(2)
+OID_AKI = '2.5.29.35'  # Authority Key Identifier
+V1 = 0  # Version 1
